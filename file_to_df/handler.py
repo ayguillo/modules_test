797343497e8615bc675e92a881a3516daac85598
ayguillo <ayguillo@student.42.fr>
2022-03-24 16:02:15 +0100

    test

--- file_to_df/handler.py
+++ file_to_df/handler.py
@@ -3,6 +3,11 @@ import pandas as pd
 def handle(req) :
     filename = req("file")
     df = pd.read_csv(filename)
+    df = df[['Description', 'Categorie 2']]
+    df = df[df['Categorie 2'].notna()]
+    df = df[df['Description'].notna()]
+    df['Description'] = df['Description'].str.lower()
+    df['Categorie 2'] = df['Categorie 2'].str.lower()
     return {
-        "dataframe" : df
+        "dataframe" : df.to_csv()
     }
